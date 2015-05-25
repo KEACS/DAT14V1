5902e28cb220f57bbaafab71e1bfdac0da41352d
Claus Bové <clbo@kea.dk>
2015-05-26 01:44:10 +0200

    test

--- 2nd_semester/22_threads/HelloThread.java
+++ 2nd_semester/22_threads/HelloThread.java
@@ -5,7 +5,7 @@ public class HelloThread extends Thread
 
 	public void run()
 	{
-		System.out.println("Hello from a thread!");
+		System.out.println("Hello from a thread!!!");
 	}
 
 	public static void main(String args[])
